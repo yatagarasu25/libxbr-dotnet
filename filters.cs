5b716456f73785e52fac05413fc64117d7d5c775
Igor Karatayev <igor.cwer@gmail.com>
2023-01-07 23:10:49 +0300

    update

--- filters.cs
+++ filters.cs
@@ -1,10 +1,41 @@
 ﻿using MathEx;
 using System.Runtime.InteropServices;
+using static libxbr_dotnet.xbr;
 
 namespace libxbr_dotnet;
+
+
+public static partial class Ex
+{
+	public static mode_xbr xm(this (mode mode, scale scale) m)
+		=> m.mode switch {
+			mode.xbr => m.scale != scale.same
+				? mode_xbr.xbr2x + ((int)m.scale - 1)
+				: mode_xbr.nil,
+			mode.hq => m.scale != scale.same
+				? mode_xbr.hq2x + ((int)m.scale - 1)
+				: mode_xbr.nil,
+			_ => throw new ArgumentOutOfRangeException()
+		};
+}
+
 public unsafe static class xbr
 {
+	public enum scale
+	{
+		same,
+		x2,
+		x3,
+		x4,
+	}
 	public enum mode
+	{
+		orig,
+		xbr,
+		hq
+	}
+
+	public enum mode_xbr
 	{
 		xbr2x,
 		xbr3x,
@@ -12,6 +43,8 @@ public unsafe static class xbr
 		hq2x,
 		hq3x,
 		hq4x,
+
+		nil = xbr3x
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
@@ -52,42 +85,35 @@ public unsafe static class xbr
 	public static extern void xbr_filter_hq4x(xbr_params* ctx);
 
 
-	public static (vec2i size, colorb[] data) filter(mode mode, colorb[] input, vec2i size, int[] xbr_data)
+
+	public static (vec2i size, colorb[] data) filter((mode mode, scale scale) m, colorb[] input, vec2i size, int[] xbr_data)
 	{
 		fixed (colorb* input_pin = &input[0])
 		fixed (int* xbr_data_pin = &xbr_data[0])
 		{
-			return filter(mode, (byte*)input_pin, size, (byte*)xbr_data_pin);
+			return filter(m, (byte*)input_pin, size, (byte*)xbr_data_pin);
 		}
 	}
 
-	public static (vec2i size, colorb[] data) filter(mode mode, colorb[] input, vec2i size, byte* xbr_data)
+	public static (vec2i size, colorb[] data) filter((mode mode, scale scale) m, colorb[] input, vec2i size, byte* xbr_data)
 	{
 		fixed (colorb* input_pin = &input[0])
 		{
-			return filter(mode, (byte*)input_pin, size, xbr_data);
+			return filter(m, (byte*)input_pin, size, xbr_data);
 		}
 	}
 
-	public static (vec2i size, colorb[] data) filter(mode mode, byte* inputb, vec2i size, int[] xbr_data)
+	public static (vec2i size, colorb[] data) filter((mode mode, scale scale) m, byte* inputb, vec2i size, int[] xbr_data)
 	{
 		fixed (int* xbr_data_pin = &xbr_data[0])
 		{
-			return filter(mode, inputb, size, (byte*)xbr_data_pin);
+			return filter(m, inputb, size, (byte*)xbr_data_pin);
 		}
 	}
 
-	public static (vec2i size, colorb[] data) filter(mode mode, byte* inputb, vec2i size, byte* xbr_data)
+	public static (vec2i size, colorb[] data) filter((mode mode, scale scale) m, byte* inputb, vec2i size, byte* xbr_data)
 	{
-		var scaleFactor = mode switch {
-				mode.xbr2x => 2,
-				mode.xbr3x => 3,
-				mode.xbr4x => 4,
-				mode.hq2x => 2,
-				mode.hq3x => 3,
-				mode.hq4x => 4,
-				_ => throw new NotImplementedException(),
-			};
+		var scaleFactor = (int)m.scale + 1;
 		var outputSize = size * scaleFactor;
 		var output = new colorb[outputSize.product];
 
@@ -105,24 +131,24 @@ public unsafe static class xbr
 				outPitch = size.x * 4 * scaleFactor
 			};
 
-			switch (mode)
+			switch (m.xm())
 			{
-				case mode.xbr2x:
+				case mode_xbr.xbr2x:
 					xbr_filter_xbr2x(&xbr_params);
 					break;
-				case mode.xbr3x:
+				case mode_xbr.xbr3x:
 					xbr_filter_xbr3x(&xbr_params);
 					break;
-				case mode.xbr4x:
+				case mode_xbr.xbr4x:
 					xbr_filter_xbr4x(&xbr_params);
 					break;
-				case mode.hq2x:
+				case mode_xbr.hq2x:
 					xbr_filter_hq2x(&xbr_params);
 					break;
-				case mode.hq3x:
+				case mode_xbr.hq3x:
 					xbr_filter_hq3x(&xbr_params);
 					break;
-				case mode.hq4x:
+				case mode_xbr.hq4x:
 					xbr_filter_hq4x(&xbr_params);
 					break;
 			}
